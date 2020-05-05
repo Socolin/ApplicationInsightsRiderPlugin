138518cda0a3a542f67be015e4fb016e6ae805ab
Socolin <provost.bertrand@gmail.com>
2020-05-05 18:46:54 -0400

    Fix for Microsoft.ApplicationInsights version 2.14. Telemetry names changed.

--- src/main/java/fr/socolin/applicationinsights/TelemetryType.java
+++ src/main/java/fr/socolin/applicationinsights/TelemetryType.java
@@ -4,27 +4,28 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public enum TelemetryType {
-    Message("Message"),
-    Request("Request"),
-    Exception("Exception"),
-    Metric("Metric"),
-    Event("Event"),
-    RemoteDependency("RemoteDependency"),
-    Unk(null);
+    Message("Message", "AppTraces"),
+    Request("Request", "AppRequests"),
+    Exception("Exception", "AppExceptions"),
+    Metric("Metric", "AppMetrics"),
+    Event("Event", "AppEvents"),
+    RemoteDependency("RemoteDependency", "AppDependencies"),
+    Unk();
 
-    @Nullable
-    private String typeName;
+    private String[] typeNames;
 
-    TelemetryType(@Nullable String typeName) {
-        this.typeName = typeName;
+    TelemetryType(@Nullable String... typeNames) {
+        this.typeNames = typeNames;
     }
 
     @NotNull
     public static TelemetryType fromType(@NotNull String typeName) {
         String name = typeName.substring(typeName.lastIndexOf('.') + 1);
         for (TelemetryType type : TelemetryType.values()) {
-            if (name.equals(type.typeName))
-                return type;
+            for (String n : type.typeNames) {
+                if (name.equals(n))
+                    return type;
+            }
         }
         return Unk;
     }
